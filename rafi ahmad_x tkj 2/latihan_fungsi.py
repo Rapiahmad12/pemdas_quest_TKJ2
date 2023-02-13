b1150a853729be3c03d294c246ab604d104383ad
Rapiahmad12 <125232997+Rapiahmad12@users.noreply.github.com>
2023-02-13 10:38:53 +0700

    Add files via upload

---
+++ rafi ahmad_x tkj 2/latihan_fungsi.py
@@ -0,0 +1,38 @@
+'''Fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+  print("Assalamu'alaikum !! Akhi Ukhty")
+
+salam()
+salam()
+salam()
+
+'''Buatlah sebuah fungsi versi kalian'''
+
+def sapa():
+  print("waalaikum salam")
+
+sapa()
+sapa()
+sapa()
+
+def say_hi(nama):
+  print("Halo perkenalkan nama saya", nama)
+
+say_hi("Jawir")
+say_hi("Reog")
+
+'''Buatlah fungsi dengan 3 Parameter'''
+
+def welcome(nama,umur,kelas):
+   print("halo nama saya:",nama)
+   print("umur",umur)
+   print("kelas",kelas)
+
+welcome("rapi", "15", "10")
+  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
+
